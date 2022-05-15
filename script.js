7a08e354df9ce723c5e0f3c0a0f43a84caae4d2e
Lenar <gabdulkhanov.lenar@mail.ru>
2022-05-16 00:14:48 +0600

    slider

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ window.onload = function() {
     var menu = document.getElementsByClassName("menu__list")[0];		
     var button = document.getElementsByClassName("open_menu")[0];			
     var lock = document.getElementsByTagName("body")[0];
-
+    var main = document.querySelector(".main");
     var images = document.querySelectorAll(".slider-line img");
     var sliderline = document.querySelector(".slider-line");
+    
     var count = 0;
     var widt;
     var timer;
@@ -12,6 +13,7 @@ window.onload = function() {
     function init() {
         widt = document.querySelector('.main__phone').offsetWidth;
         console.log(widt);
+        main.style.height = 'auto';
         sliderline.style.width = widt * images.length + "px";
         images.forEach(item => {
             item.style.width = widt + 'px';
